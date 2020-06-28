d1c957395e1aa92e68136db57360cd961fa6bde1
Mane-X <blacciboy@gmail.com>
2020-06-28 03:00:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+
+
+class App extends React.Component {
+  constructor(props) {
+    super (props);
+    this.state ={result:''};
+  }
+
+  render () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <form className="form">
+          <h1>My Calculator</h1>
+          <div className="calculator"/>
+            <input className="text" value={this.state.result} id="txtview" type="" ></input>
+            <table>
+              <tr>
+                <td><input type="button" value="(" onClick={(e)=>this.onClick(e.target.value)} /></td>
+                <td><input type="button" value=")" onClick={(e)=>this.onClick(e.target.value)} /></td>
+                <td><input type="button" value="%"  onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="AC" onClick={(e)=>this.onClick(e.target.value)}/></td>
+              </tr>
+              <tr>
+                <td><input type="button" value="7" onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="8" onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="9" onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="-" onClick={(e)=>this.onClick(e.target.value)}/></td>
+              </tr>
+              <tr>
+                <td><input type="button" value="4" onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="5" onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="6" onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="+" onClick={(e)=>this.onClick(e.target.value)}/></td>
+              </tr>
+              <tr>
+                <td><input type="button" value="1" onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="2"  onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="3"  onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="/" onClick={(e)=>this.onClick(e.target.value)}/></td>
+              </tr>					
+              <tr>
+                <td><input type="button" value="0" onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="." onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="=" onClick={(e)=>this.onClick(e.target.value)}/></td>
+                <td><input type="button" value="x" onClick={(e)=>this.onClick(e.target.value)}/></td>
+              </tr>
+				</table>
+        </form>
+        </div>
   );
+}
+onClick = button => {
+
+  if(button === "="){
+      this.calculate()
+  }
+
+  else if(button === "AC"){
+      this.reset()
+  }
+  else if(button === "CE"){
+      this.backspace()
+  }
+
+  else {
+      this.setState({
+          result: this.state.result + button
+      })
+  }
+};
+reset = () => {
+  this.setState({
+      result: ""
+  })
+};
+calculate = () => {
+  try {
+      this.setState({
+          // eslint-disable-next-line
+          result: (eval(this.state.result) || "" ) + ""
+      })
+  } catch (e) {
+      this.setState({
+          result: "Math error"
+      })
+
+  }
+};
+
+
 }
 
 export default App;
